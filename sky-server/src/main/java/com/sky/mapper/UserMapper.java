5f3cab41b652464d927cd635a82e32bd7f4a2849
xxxace <731162160@qq.com>
2023-07-20 11:34:48 +0800

    添加了订单支付和微信支付代码

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -10,6 +10,8 @@ public interface UserMapper {
     @Select("select * from user where openid = #{openid}")
     User getUserByOpenid(String openid);
 
+    @Select("select * from user where id = #{id}")
+    User getById(Long id);
 
     void insert(User user);
 }
